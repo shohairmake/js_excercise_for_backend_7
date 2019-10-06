08c1d88fd5ba16855a2f97006e6393b5b17f8d5c
sho.tanaka <sho.tanaka@aijus.com>
2019-10-06 20:11:58 +0900

    created a Todo.findAll method

---
+++ test/models/Todos/findAll.test.js
@@ -0,0 +1,19 @@
+const assert = require('power-assert');
+const Todo = require('../../../models/Todo');
+
+describe('Todo.findAll', () => {
+    it('配列の決められたデータ構造でTodoが格納されている', () => {
+        const todos = Todo.findAll();
+        assert.strictEqual(Array.isArray(todos), true);
+        assert.strictEqual(todos.length > 0, true);
+        // todos.forEach(todo => {
+        //     assert.deepStrictEqual({...todo}, {
+        //         id: todo.id,
+        //         title: todo.title,
+        //         body: todo.body,
+        //         createdAt: todo.createdAt,
+        //         updatedAt: todo.updatedAt,
+        //     });
+        // });
+    });
+});
